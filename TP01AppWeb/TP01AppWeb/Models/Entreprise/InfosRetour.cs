6d729ad4f205486cbcdc0cdf8526caf45110189d
Xavier Hivon-Lefebvre <70168674+Elrys@users.noreply.github.com>
2021-05-14 17:27:05 -0400

    InfosRetour Fin

--- TP01AppWeb/TP01AppWeb/Models/Entreprise/InfosRetour.cs
+++ TP01AppWeb/TP01AppWeb/Models/Entreprise/InfosRetour.cs
@@ -8,12 +8,21 @@ namespace TP01AppWeb.Models.Entreprise
 {
     public class InfosRetour : ReadMe
     {
+        [Display(Name = "Date de location")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
         public DateTime DateLocation { get; set; }
+        [Display(Name = "Nombre de jours de location")]
         public int? JoursLocation { get; set; }
+        [Display(Name = "Code de la succursale")]
         public int? SuccursaleId { get; set; }
+        [Display(Name = "Numéro de permis")]
         public string NoPermis { get; set; }
         public string Nom { get; set; }
+        [Display(Name = "Prénom")]
         public string Prenom { get; set; }
+        [Display(Name = "Numéro de télépĥone")]
         public string NoTelephone { get; set; }
+        public bool Accident { get; set; }
     }
 }
